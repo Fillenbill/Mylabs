9f2c9f0d08245f0ccd6acf6fb69b4d6d42873346
Fillenbill <96179478+Fillenbill@users.noreply.github.com>
2023-11-13 14:15:43 +0300

    Add files via upload

---
+++ lol/expr.cpp
@@ -0,0 +1,135 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+#include <chrono>
+
+HANDLE ThreadArray[100] = { };
+HANDLE Event = NULL;
+unsigned short ThreadCount = 0;
+unsigned short SplitNumber = 0;
+unsigned int Result = 0;
+int* CurNumber = 0;
+int i = 0;
+int Index = 0;
+
+DWORD WINAPI ThreadFunction(void* parameterVoid) {
+    WaitForSingleObject(Event, INFINITE);
+    int Count = 0;
+    while (TRUE) {
+        Result++;
+        //Проверка, что число уже разложено -> Выход
+        if (CurNumber[0] == 1) {
+            SetEvent(Event);
+            return 0;
+        }
+
+        //Проверка на то, что поток выполнил все свои разложения -> Выход
+        if (i == 0) {
+            Count++;
+            if (Count == Index) {
+                SetEvent(Event);
+                return 0;
+            }
+        }
+        //Количество единиц сверху
+        int Val = 0;
+        while (i >= 0 && CurNumber[i] == 1) {
+            Val++;
+            i--;
+        }
+        //Если все единицы -> Выход
+        if (i < 0) {
+            SetEvent(Event);
+            return 0;
+        }
+        //Вычитаем из не единицы
+        CurNumber[i]--;
+        Val++;
+        //
+        while (Val > CurNumber[i]) {
+            CurNumber[i + 1] = CurNumber[i];
+            Val -= CurNumber[i];
+            i++;
+        }
+        i++;
+        CurNumber[i] = Val;
+        for (int k = i + 1; k < SplitNumber; k++) CurNumber[k] = 0;
+        //for (int k = 0; CurNumber[k] > 0; k++)printf("%d", CurNumber[k]);
+        //printf("\n");
+    }
+}
+
+
+//Функция для чтения данных из файла
+void ReadInputFile() {
+    FILE* InputFile = fopen("input.txt", "r");
+    if (InputFile == NULL) {
+        printf("Error: failed to open input file.\n");
+        exit(1);
+    }
+    if (!fscanf(InputFile, "%hu%hu", &ThreadCount, &SplitNumber)) {
+        //Вывод ошибки
+        exit(1);
+    }
+    fclose(InputFile);
+}
+//Функция для записи данных в файл
+void write(time_t Time) {
+    FILE* OutputFile = fopen("output.txt", "w");
+    FILE* TimeFile = fopen("time.txt", "w");
+
+
+    fprintf(OutputFile, "%u\n", ThreadCount);
+    fprintf(OutputFile, "%u\n", SplitNumber);
+    fprintf(OutputFile, "%u\n", Result - ThreadCount);
+    fprintf(TimeFile, "%lf", ((double)Time) / CLOCKS_PER_SEC);
+
+    fclose(TimeFile);
+    fclose(OutputFile);
+
+}
+void Close() {
+    for (i = 0; i < ThreadCount; i++)
+        CloseHandle(ThreadArray[i]);
+    if (Event != 0)
+        CloseHandle(Event);
+}
+void Create() {
+    for (int k = 0; k < ThreadCount; k++)
+        ThreadArray[k] = CreateThread(0, 0, ThreadFunction, (char*)0 + k, 0, 0);
+}
+void GetIndex() {
+    Index = SplitNumber - 1;
+    if (ThreadCount >= Index) ThreadCount = Index - 1;
+    if (Index % ThreadCount == 0) Index = Index / ThreadCount;
+    else Index = (Index / ThreadCount) + 1;
+}
+void ActionMain() {
+    //Чтение входных данных
+    ReadInputFile();
+    //Создание начальной последовательности
+    CurNumber = new int[SplitNumber];
+    CurNumber[i] = SplitNumber;
+    //Получение количества действий для одного потока
+    GetIndex();
+    //Создание потоков
+    Create();
+    clock_t Time = clock();
+    // Ожидание завершения всех потоков
+    while (TRUE)
+        if (WAIT_TIMEOUT != WaitForMultipleObjects(ThreadCount, ThreadArray, TRUE, 0)) break;
+    Time = clock() - Time;
+    delete[] CurNumber;
+    //Запись в файл результатов
+    write(Time);
+}
+
+int main(void) {
+    Event = CreateEvent(NULL, FALSE, TRUE, NULL);
+    // Запускаем выполнение программы
+    ActionMain();
+    //
+    Close();
+    return 0;
+}
+
---
+++ lol/qsort.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <thread>
+#include <queue>
+#include <windows.h>
+
+using namespace std;
+
+struct Subtask {
+    int start;
+    int end;
+};
+
+int thread_count;  // Количество потоков
+int array_size;    // Размер массива
+vector<int> array_; // Массив, который будем сортировать
+HANDLE task_semaphore; // Семафор для управления задачами
+CRITICAL_SECTION critical_section; // Критическая секция для синхронизации доступа к общим данным
+queue<Subtask> task_queue; // Очередь задач для потоков
+
+void QuickSort(int low, int high); // Прототип функции быстрой сортировки
+void EnqueueTask(Subtask task); // Прототип функции добавления задачи в очередь
+bool DequeueTask(Subtask& task); // Прототип функции извлечения задачи из очереди
+void NotifyAllThreads(); // Прототип функции оповещения всех потоков
+
+DWORD WINAPI ThreadQuickSort(LPVOID lpParam); // Прототип функции потока быстрой сортировки
+
+DWORD WINAPI ThreadQuickSort(LPVOID lpParam) {
+    Subtask task;
+
+    while (DequeueTask(task)) {
+        QuickSort(task.start, task.end); // Выполнение быстрой сортировки для задачи
+    }
+
+    return 0;
+}
+
+void QuickSort(int low, int high) {
+    int i = low;
+    int j = high;
+    int pivot = array_[(low + high) / 2];
+
+    while (i <= j) {
+        while (array_[i] < pivot) {
+            i++;
+        }
+        while (array_[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(array_[i++], array_[j--]); // Обмен элементов местами
+        }
+    }
+
+    if (low < j) {
+        Subtask left_task = { low, j };
+        EnqueueTask(left_task); // Добавление левой части массива в очередь
+    }
+    if (i < high) {
+        Subtask right_task = { i, high };
+        EnqueueTask(right_task); // Добавление правой части массива в очередь
+    }
+}
+
+void EnqueueTask(Subtask task) {
+    EnterCriticalSection(&critical_section); // Вход в критическую секцию
+    task_queue.push(task); // Добавление задачи в очередь
+    LeaveCriticalSection(&critical_section); // Выход из критической секции
+}
+
+bool DequeueTask(Subtask& task) {
+    EnterCriticalSection(&critical_section); // Вход в критическую секцию
+
+    if (!task_queue.empty()) {
+        task = task_queue.front(); // Извлечение задачи из очереди
+        task_queue.pop();
+        LeaveCriticalSection(&critical_section); // Выход из критической секции
+        return true;
+    }
+
+    LeaveCriticalSection(&critical_section); // Выход из критической секции
+    return false;
+}
+
+int main() {
+    ifstream input_file("input.txt");
+    ofstream output_file("output.txt");
+    ofstream time_file("time.txt");
+
+    input_file >> thread_count >> array_size;
+
+    array_.resize(array_size);
+    for (int i = 0; i < array_size; ++i) {
+        input_file >> array_[i];
+    }
+
+    task_semaphore = CreateSemaphore(NULL, 1, thread_count, NULL); // Создание семафора
+    InitializeCriticalSection(&critical_section); // Инициализация критической секции
+
+    vector<HANDLE> threads(thread_count);
+    for (int i = 0; i < thread_count; ++i) {
+        threads[i] = CreateThread(NULL, 0, ThreadQuickSort, NULL, 0, NULL); // Создание потоков
+    }
+
+    DWORD start_time = GetTickCount(); // Запуск таймера
+
+    Subtask initial_task = { 0, array_size - 1 };
+    EnqueueTask(initial_task); // Добавление начальной задачи в очередь
+
+    WaitForMultipleObjects(thread_count, threads.data(), TRUE, INFINITE); // Ожидание завершения потоков
+
+    DWORD end_time = GetTickCount(); // Остановка таймера
+    DWORD duration = end_time - start_time; // Вычисление длительности выполнения
+
+    output_file << thread_count << "\n" << array_size << "\n";
+    for (int i = 0; i < array_size; ++i) {
+        output_file << array_[i] << " "; // Запись отсортированного массива в файл
+    }
+
+    time_file << duration; // Запись времени выполнения в файл
+
+    CloseHandle(task_semaphore); // Закрытие семафора
+    DeleteCriticalSection(&critical_section); // Уничтожение критической секции
+
+    input_file.close();
+    output_file.close();
+    time_file.close();
+
+    return 0;
+}
